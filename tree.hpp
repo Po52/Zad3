94b6e4fe785cf6134efb77362f04c24d03c91ecb
Po52 <131165561+Po52@users.noreply.github.com>
2023-05-23 23:49:46 +0200

    Update tree.hpp

--- tree.hpp
+++ tree.hpp
@@ -1,15 +1,29 @@
-class Tree{
-private :
+#ifndef SHAPE_TREE_HPP
+#define SHAPE_TREE_HPP
+
+
+class tree {
+private:
     int height;
     char symbol;
     string color;
-    int **tab;         // matryca
-    int x, y;         //pozycja drzewa w lesie
-public :
-    Tree(int h, char s, string c);
-    ~Tree();
+    int **tab;
+
+
+public:
+
     void printTree();
 
+    tree(int h, char s, string c);
+    ~tree();
+
+    void printTreeFromTab();
+    int getHeight();
+    int getWidth();
+
     friend class Forest;
+
 };
 
+
+#endif //SHAPE_TREE_HPP
