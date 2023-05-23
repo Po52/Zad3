07e64369e6a1ddf02c195bad6177a29e6b466f84
Po52 <131165561+Po52@users.noreply.github.com>
2023-05-23 23:46:58 +0200

    Update rectangle.hpp

--- rectangle.hpp
+++ rectangle.hpp
@@ -1,14 +1,15 @@
-#include "Shape.hpp";
+#ifndef SHAPE_RECTANGLE_HPP
+#define SHAPE_RECTANGLE_HPP
 
-class Rectangle : public Shape
-{
-    public:
-       using Shape::Shape;
-    
+class Rectangle : public shape {
+
+public:
+    using shape::shape;
+
+    virtual void Fill();
+    Rectangle(int h, int w, char s, string c);
+    Rectangle(int h, char s, string c);
 };
 
 
-
-
-  //Rectangle(); 
-  //Rectangle(int h, char s, string c);
+#endif //SHAPE_RECTANGLE_HPP
